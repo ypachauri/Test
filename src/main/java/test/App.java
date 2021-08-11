b55746babd58dbf68c641fe1670dc89480b4c9cc
ypachauri <yashpachauri28@gmail.com>
2021-08-11 14:57:30 +0530

    updated

--- src/main/java/test/App.java
+++ src/main/java/test/App.java
@@ -1,14 +1,5 @@
 package test;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 /**
  * Hello world!
  *
@@ -17,36 +8,12 @@ public class App
 {
 	static void Test()
     {
-//    	ExecutorService executorService = Executors.newCachedThreadPool();
-//    	ExecutorService executorService2 = Executors.newFixedThreadPool(5);
-//    	ExecutorService executorService3 = Executors.newSingleThreadScheduledExecutor();
-    	ExecutorService executorService4 = Executors.newSingleThreadExecutor();
-    	Runnable run = () -> {
-    		System.err.println("Running...");
-    	};
-    	executorService4.execute(run);
-    	System.err.println(executorService4.submit(run));
-    }
-    
-    static void TestJava8()
-    {
-    	List<Integer> list = Arrays.asList(2,5,3,6,9,1,7,4,0,8);
-    	Object result = list.stream().mapToInt(a -> a).sum();
-    	result = list.stream().max(Comparator.comparing(Integer::intValue));
-    	result = list.stream().collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()));
-    	
-    	List<String> items =
-                Arrays.asList("apple", "apple", "banana",
-                        "apple", "orange", "banana", "papaya");
-    	result = items.stream().collect(Collectors.groupingBy(Function.identity(), Collectors.counting()));
-    	
-    	System.err.println(result);
+		
     }
   
     /* program to test above function */
     public static void main(String args[]) 
     { 
-    	TestJava8();
-//    	Test();
+    	Test();
     }
 }
